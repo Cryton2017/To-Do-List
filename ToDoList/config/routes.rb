9c8e874acd0e514fc11a43f5667faba9e1416405
Christopher Littlewood <christopher.littlewood@connect.qut.edu.au>
2020-03-12 08:40:36 +1000

    Setup Basic Navigation
    Basic navigation has been setup.

--- ToDoList/config/routes.rb
+++ ToDoList/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html 
+  get 'login', to: 'login#login'
+  get 'dashboard', to: 'dashboard#dashboard'
+
+  root to: 'landing#index'
 end
